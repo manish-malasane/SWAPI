4aa78c591c16cf33115e7047eb849d1d86fe4258
Manish Malasane <mmalasane6@gmail.com>
2023-03-02 19:19:00 +0530

    all the updates are done which are related with task_four

---
+++ not_necessary_db_conn.py
@@ -0,0 +1,50 @@
+"""
+user: root
+password: qwerty
+host: 127.0.0.1
+port: 3306
+database: starwarsDB
+"""
+from pprint import pprint
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             port=3306,
+                             password='Aaibaba',
+                             database='meDB',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "SELECT * FROM meDB.People"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        pprint(result)
+    connection.commit()
+
+# """
+# user: root
+# password: qwerty
+# host: 127.0.0.1
+# port: 3306
+# database: starwarsDB
+# """
+#
+# import pymysql.cursors
+#
+# # Connect to the database
+# connection = pymysql.connect(host='127.0.0.1',
+#                              user='root',
+#                              port=3306,
+#                              password='Aaibaba',
+#                              database='starwarsDB',
+#                              cursorclass=pymysql.cursors.DictCursor)
+#
+# cursor = connection.cursor()
+# cursor.execute("SHOW DATABASES")
+# results = cursor.fetchall()
+# for result in results:
+#     print(result)
